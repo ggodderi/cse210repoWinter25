3d71f7d825b8e654e3ab9d036fbf49e95eb9299b
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-02-24 11:07:09 -0700

    Practice name changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Bob's World!");
+        Console.WriteLine("Input your name: ");
+
+        // int name = Console.Read();
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"{name}");
+
     }
+
 }
