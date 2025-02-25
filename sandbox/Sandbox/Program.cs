cfaaedb60c1d5b85a56c35c0760ec5c7d9b848db
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-02-25 09:49:25 -0700

    Added Input LIne

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,8 @@ class Program
     {
         Console.WriteLine("Hello Bob's World!");
         Console.WriteLine("Hello CSE 210");
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}");
     }
 }
